b77682598061942791ffdcfe844f0186a31db06b
Duyen Rice <duyenqt98@gmail.com>
2020-06-11 19:53:21 +0700

    Add Account Screen

--- screens/YourAccount.js
+++ screens/YourAccount.js
@@ -1,67 +1,201 @@
 import React, {Component} from 'react';
-import { TextInput, Text, View, TouchableHighlight, Button } from 'react-native';
+import {
+  TextInput,
+  Text,
+  View,
+  TouchableHighlight,
+  Button,
+  Image,
+} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
-export default class ListScreen extends Component {
-    constructor(props) {
-        super(props);
+export default class YourAccount extends Component {
+  constructor(props) {
+    super(props);
+  }
+  removeItemValue = async () => {
+    try {
+      await AsyncStorage.removeItem('user');
+      alert('â');
+      return true;
+    } catch (exception) {
+      console.log(exception);
     }
-    removeItemValue = async() => {
-        try {
-            await AsyncStorage.removeItem('user');
-            alert('â');
-            return true;
-            
-        }
-        catch(exception) {
-            console.log(exception);
-        }
-    }
-    render() {
-        return (
-            <View style={{flex: 1, flexDirection: 'column'}}>
-                <Text style={{fontSize: 16, textAlign: 'center', marginTop: 10, marginBottom: 10, letterSpacing: 1}}>Tài khoản của bạn</Text> 
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+        }}>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            paddingVertical: 10,
+            letterSpacing: 1,
+            backgroundColor: '#eee',
+          }}>
+          Tài khoản
+        </Text>
+        <View
+          style={{
+            marginRight: 10,
+            marginLeft: 10,
+            paddingTop: 10,
+            paddingBottom: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <View>
+            <Text style={{fontSize: 15, marginBottom: 10}}>
+              Tên chủ tài khoản
+            </Text>
+            <Text style={{fontSize: 18}}>Nguyễn Văn A</Text>
+          </View>
 
-                <Text style={{fontSize: 15, marginRight: 10, marginLeft: 10, marginTop: 10}}>Họ và tên: Nguyễn Văn A</Text>
-                <Text style={{fontSize: 15, marginRight: 10, marginLeft: 10, marginTop: 10}}>Địa chỉ: Đà Nẵng</Text>
-                <Text style={{fontSize: 15, marginRight: 10, marginLeft: 10, marginTop: 10}}>Khu vực của bạn: Đà Nẵng</Text>
-                <Text style={{fontSize: 15, marginRight: 10, marginLeft: 10, marginTop: 10}}>Số điện thoại: 0335941792</Text>
-                <Text style={{fontSize: 15, marginRight: 10, marginLeft: 10, marginTop: 10}}>Email: giakhanh9890@gmail.com</Text>
+          <Image
+            source={require('../images/room.jpg')}
+            style={{width: 60, height: 60, borderRadius: 30}}
+          />
+        </View>
 
-                <TouchableHighlight
-                    style={{marginTop: 20, marginBottom: 10, marginLeft: 10, marginRight: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: 'lightblue', borderRadius: 10}}
-                    onPress={() => {
-                        this.props.navigation.navigate('EditAccountScreen');
-                    }}
-                >
-                    <Text style={{textAlign: 'center'}}>Chỉnh sửa thông tin cá nhân</Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={{marginBottom: 10, marginLeft: 10, marginRight: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: 'lightblue', borderRadius: 10}}
-                    onPress={() => {
-                        this.props.navigation.navigate('EditPasswordAccountScreen');
-                    }}
-                >
-                    <Text style={{textAlign: 'center'}}>Chỉnh sửa mật khẩu</Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={{marginBottom: 10, marginLeft: 10, marginRight: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: 'lightblue', borderRadius: 10}}
-                    onPress={() => {
-                        this.props.navigation.navigate('FeedbackScreen');
-                    }}
-                >
-                    <Text style={{textAlign: 'center'}}>Phản hồi cho quản trị viên</Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={{marginBottom: 10, marginLeft: 10, marginRight: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: 'lightblue', borderRadius: 10}}
-                    onPress={() => {
-                        this.removeItemValue();
-                    }}
-                >
-                    <Text style={{textAlign: 'center'}}>Đăng xuất</Text>
-                </TouchableHighlight>
-            </View>
-        )
-        
-    }
+        <View
+          style={{
+            marginRight: 10,
+            marginLeft: 10,
+            marginTop: 10,
+            paddingBottom: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc',
+          }}>
+          <Text style={{fontSize: 15, marginBottom: 10}}>Email</Text>
+          <Text style={{fontSize: 18}}>test@gmail.com</Text>
+        </View>
+
+        <View
+          style={{
+            marginRight: 10,
+            marginLeft: 10,
+            marginTop: 10,
+            paddingBottom: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc',
+          }}>
+          <Text style={{fontSize: 15, marginBottom: 10}}>Số điện thoại</Text>
+          <Text style={{fontSize: 18}}>0335941792</Text>
+        </View>
+
+        <View
+          style={{
+            marginRight: 10,
+            marginLeft: 10,
+            marginTop: 10,
+            paddingBottom: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc',
+          }}>
+          <Text style={{fontSize: 15, marginBottom: 10}}>Địa chỉ</Text>
+          <Text style={{fontSize: 18}}>Đà Nẵng</Text>
+        </View>
+
+        <TouchableHighlight
+          underlayColor="transparent"
+          style={{
+            marginVertical: 10,
+          }}
+          onPress={() => {
+            this.props.navigation.navigate('EditAccountScreen');
+          }}>
+          <View
+            style={{
+              marginHorizontal: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{color: '#e88a59', fontWeight: 'bold', fontSize: 16}}>
+              Chỉnh sửa tài khoản
+            </Text>
+            <Image
+              source={require('../images/next.png')}
+              style={{width: 15, height: 15}}
+            />
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="transparent"
+          style={{
+            marginVertical: 10,
+          }}
+          onPress={() => {
+            this.props.navigation.navigate('EditPasswordAccountScreen');
+          }}>
+          <View
+            style={{
+              marginHorizontal: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{color: '#e88a59', fontWeight: 'bold', fontSize: 16}}>
+              Chỉnh sửa mật khẩu
+            </Text>
+            <Image
+              source={require('../images/next.png')}
+              style={{width: 15, height: 15}}
+            />
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="transparent"
+          style={{
+            marginVertical: 10,
+          }}
+          onPress={() => {
+            this.props.navigation.navigate('FeedbackScreen');
+          }}>
+          <View
+            style={{
+              marginHorizontal: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{color: '#e88a59', fontWeight: 'bold', fontSize: 16}}>
+              Phản hồi cho quản trị viên
+            </Text>
+            <Image
+              source={require('../images/next.png')}
+              style={{width: 15, height: 15}}
+            />
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="transparent"
+          style={{
+            marginVertical: 10,
+          }}
+          onPress={() => {
+            this.removeItemValue();
+          }}>
+          <View
+            style={{
+              marginHorizontal: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{color: '#e88a59', fontWeight: 'bold', fontSize: 16}}>
+              Đăng xuất
+            </Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
