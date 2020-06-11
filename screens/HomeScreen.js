13ebf43d936ed0da612f6345a69cefceb3c7431f
Duyen Rice <duyenqt98@gmail.com>
2020-06-11 19:50:22 +0700

    Add Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
-import { Text, View, Picker, Button } from 'react-native';
-import FlatList1 from './FlatList1';
+import {Text, View, Picker, Button, ScrollView} from 'react-native';
+import Carousel from '../components/Carousel';
+import {dummyData} from '../data/DataCarousel';
+import HomeFlatList from '../components/HomeFlatList';
 //import { Dropdown } from 'react-native-material-dropdown';
 export default class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        
-        return (
-            <View style={{flex: 1, flexDirection: 'column'}}>
-                <FlatList1 navigation={this.props.navigation} title="Phòng trọ HOT"></FlatList1>
-            </View>
-           
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'column', backgroundColor: '#fff'}}>
+        <Carousel data={dummyData} />
+        <HomeFlatList
+          navigation={this.props.navigation}
+          title="Phòng trọ HOT"
+        />
+      </View>
+    );
+  }
 }
