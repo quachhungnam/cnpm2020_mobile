aa5bc94d6d733e9052dbbf379ffcf46d287d2c83
Duyen Rice <duyenqt98@gmail.com>
2020-06-11 19:46:50 +0700

    Add data folder

---
+++ data/DataCarousel.js
@@ -0,0 +1,24 @@
+export const dummyData = [
+  {
+    title: 'Căn hộ',
+    url: 'https://i.ibb.co/hYjK44F/anise-aroma-art-bazaar-277253.jpg',
+    description:
+      'Cho thuê căn hộ mini, căn hộ giá rẻ, căn hộ dịch vụ, chính chủ, đầy đủ tiện nghi, nhiều mức giá, diện tích cho thuê khác nhau.',
+    id: 1,
+  },
+  {
+    title: 'Nhà nguyên căn',
+    url: 'https://i.ibb.co/JtS24qP/food-inside-bowl-1854037.jpg',
+    description:
+      'Cho thuê nhà nguyên căn, giá rẻ, chính chủ, miễn trung gian, đầy đủ tiện nghi, mức giá, diện tích cho thuê khác nhau.',
+    id: 2,
+  },
+  {
+    title: 'Phòng cho thuê',
+    url:
+      'https://i.ibb.co/JxykVBt/flat-lay-photography-of-vegetable-salad-on-plate-1640777.jpg',
+    description:
+      'Cho thuê phòng trọ, phòng trọ sinh viên, phòng trọ cao cấp, giá tốt, đầy đủ tiện nghi.',
+    id: 3,
+  },
+];
