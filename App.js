7469ac8287f356e07c159dce573a5cd7914f2f4e
Duyen Rice <duyenqt98@gmail.com>
2020-06-11 20:09:04 +0700

    Add rest files

--- App.js
+++ App.js
@@ -7,31 +7,13 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MyTabs from './screens/MyTabs';
+import {NavigationContainer} from '@react-navigation/native';
+import MyTabs from './components/MyTabs';
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs></MyTabs>
+      <MyTabs />
     </NavigationContainer>
   );
 }
-
