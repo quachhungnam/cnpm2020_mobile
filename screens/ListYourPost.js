783212eeb0946d443673637426817dc23417fb78
Duyen Rice <duyenqt98@gmail.com>
2020-06-11 19:52:08 +0700

    Add Your Post Screen

--- screens/ListYourPost.js
+++ screens/ListYourPost.js
@@ -1,25 +1,35 @@
 import React, {Component} from 'react';
-import { TextInput, Text, View, TouchableHighlight, Button } from 'react-native';
-import FlatList2 from './FlatList2';
-export default class ListScreen extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <View style={{flex: 1, flexDirection: 'column'}}>
-                
-                <FlatList2 navigation={this.props.navigation} title="Danh sách tin đăng của bạn"></FlatList2>
-                <TouchableHighlight
-                    style={{marginLeft: 10, marginRight: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: 'lightblue', borderRadius: 10}}
-                    onPress={() => {
-                        this.props.navigation.navigate('AddPostScreen');
-                    }}
-                >
-                    <Text style={{textAlign: 'center'}}>Thêm bài đăng</Text>
-                </TouchableHighlight>
-            </View>
-        )
-        
-    }
+import {Text, View, TouchableHighlight, ScrollView} from 'react-native';
+import PostFlatList from '../components/PostFlatList';
+export default class ListYourPost extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ScrollView
+        style={{flex: 1, flexDirection: 'column', backgroundColor: '#fff'}}>
+        <PostFlatList
+          navigation={this.props.navigation}
+          title="Danh sách tin đăng của bạn"
+        />
+        <TouchableHighlight
+          underlayColor="#ffceb588"
+          style={{
+            marginLeft: 10,
+            marginRight: 10,
+            paddingTop: 10,
+            paddingBottom: 10,
+            backgroundColor: '#ffceb5',
+            borderRadius: 8,
+            marginBottom: 30,
+          }}
+          onPress={() => {
+            this.props.navigation.navigate('AddPostScreen');
+          }}>
+          <Text style={{textAlign: 'center'}}>Thêm tin đăng</Text>
+        </TouchableHighlight>
+      </ScrollView>
+    );
+  }
 }
