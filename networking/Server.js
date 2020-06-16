de27735336f9526c225121d83257300c011157f3
Quach Nam <quachhungnam@gmail.com>
2020-06-16 16:44:44 +0700

    add send feedback and change password

--- networking/Server.js
+++ networking/Server.js
@@ -5,7 +5,8 @@ const api_rates = `${yourIP}:3000/rates`
 const api_provinces = `${yourIP}:3000/provinces`
 const api_districts = `${yourIP}:3000/districts`
 const api_posttypes = `${yourIP}:3000/posttypes`
-const api_account = `${yourIP}:3000/accounts`
+const api_accounts = `${yourIP}:3000/accounts`
+const api_feedbacks = `${yourIP}:3000/feedbacks`
 
 async function getPostsFromServer() {
   try {
@@ -68,7 +69,7 @@ async function get_district_with_province(provinceCode) {
 }
 async function login(user) {
   try {
-    let result = await fetch(`${api_account}/login`, {
+    let result = await fetch(`${api_accounts}/login`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -89,7 +90,7 @@ async function login(user) {
 
 async function signup(user) {
   try {
-    let result = await fetch(`${api_account}/signup`, {
+    let result = await fetch(`${api_accounts}/signup`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -113,7 +114,7 @@ async function signup(user) {
 //GET INFOR
 async function get_account_infor(user_token) {
   try {
-    let result = await fetch(`${api_account}/usertoken/yes`, {
+    let result = await fetch(`${api_accounts}/usertoken/yes`, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -158,7 +159,7 @@ async function post_post(new_post) {
 
 async function update_account_infor(user, user_token) {
   try {
-    let result = await fetch(`${api_account}/${user._id}`, {
+    let result = await fetch(`${api_accounts}/${user._id}`, {
       method: 'PATCH',
       headers: {
         Accept: 'application/json',
@@ -170,6 +171,66 @@ async function update_account_infor(user, user_token) {
         email: user.email,
         mobile: user.mobile,
         address: user.address
+      })
+    })
+    let resultJson = await result.json()
+    return resultJson
+  } catch (err) {
+    console.log(`Error is: ${err}`)
+  }
+}
+async function update_account_password(user, user_token) {
+  try {
+    let result = await fetch(`${api_accounts}/${user._id}/password`, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        Authorization: user_token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        password: user.password,
+      })
+    })
+    let resultJson = await result.json()
+    return resultJson
+  } catch (err) {
+    console.log(`Error is: ${err}`)
+  }
+}
+async function check_account_password(user, user_token) {
+  try {
+    let result = await fetch(`${api_accounts}/${user._id}/checkpassword`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: user_token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        password: user.password
+      })
+    })
+    let resultJson = await result.json()
+    return resultJson
+  } catch (err) {
+    console.log(`Error is: ${err}`)
+  }
+}
+
+
+async function send_feed_back(user, user_token, feedback) {
+  try {
+    let result = await fetch(`${api_feedbacks}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: user_token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        accountId: user._id,
+        description: feedback,
       })
     })
     let resultJson = await result.json()
@@ -193,4 +254,7 @@ export { signup }
 export { get_account_infor }
 export { post_post }
 export { update_account_infor }
+export { check_account_password }
+export { update_account_password }
+export { send_feed_back }
 
--- screens/Feedback.js
+++ screens/Feedback.js
@@ -1,44 +1,93 @@
-import React, { Component } from 'react';
-import { TextInput, Text, View, TouchableHighlight, Button } from 'react-native';
-export default class Feedback extends Component {
-  constructor(props) {
-    super(props);
+import React, { Component, useEffect, useState } from 'react';
+import { TextInput, Text, StyleSheet, ScrollView, View, TouchableHighlight, Button } from 'react-native';
+import { send_feed_back } from '../networking/Server'
+export default function Feedback(props) {
+  const [feedback, set_feedback] = useState('')
+  const [account_infor, set_account_infor] = useState({ _id: 1 })
+  const [account_token, set_account_token] = useState('')
+
+  useEffect(() => {
+    get_account_infor()
+  }, [])
+
+  const send_feedback = () => {
+    try {
+      send_feed_back(account_infor, account_token, feedback).then(res => {
+        if (res.success == true) {
+          alert('Cảm ơn bạn đã gửi phản hồi')
+          props.navigation.navigate('YourAccountScreen')
+          return
+        }
+        if (res.success == false) {
+          alert('Không thể gửi phản hồi')
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
-  render() {
-    return (
-      <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#fff' }}>
-        <Text style={{ marginRight: 10, marginLeft: 10, marginTop: 10 }}>
-          Vấn đề bạn cần phản hồi
-        </Text>
-        <TextInput
-          style={{
-            marginRight: 10,
-            marginLeft: 10,
-            padding: 10,
-            marginTop: 10,
-            borderRadius: 8,
-            backgroundColor: '#fff',
-            borderColor: '#333',
-            borderWidth: 1,
-          }}
-          multiline={true}
-          numberOfLines={6}
-          autoCorrect={false}
-        />
-        <TouchableHighlight
-          style={{
-            marginBottom: 10,
-            marginTop: 20,
-            marginLeft: 10,
-            marginRight: 10,
-            paddingTop: 10,
-            paddingBottom: 10,
-            backgroundColor: '#ffceb5',
-            borderRadius: 8,
-          }}>
-          <Text style={{ textAlign: 'center' }}>Gửi phản hồi</Text>
-        </TouchableHighlight>
-      </View>
-    );
+  const get_account_infor = () => {
+    const { account, user_token } = props.route.params
+    set_account_infor({
+      _id: account._id,
+    })
+    set_account_token(user_token)
   }
+
+  return (
+    <ScrollView
+      style={{ flex: 1, flexDirection: 'column', backgroundColor: '#fff' }}>
+      <Text style={styles.txt_pass}>
+        Phản hồi của bạn
+      </Text>
+      <TextInput
+        style={styles.input_pass}
+        returnKeyType="next"
+        secureTextEntry
+        autoCorrect={false}
+        multiline={true}
+        numberOfLines={6}
+        onChangeText={(text) => {
+          set_feedback(text)
+        }}
+      />
+
+      <TouchableHighlight
+        underlayColor={color_touch}
+        style={styles.touch_changepass}
+        onPress={() => {
+          send_feedback()
+        }}
+      >
+        <Text style={{ textAlign: 'center' }}>Gửi phản hồi</Text>
+      </TouchableHighlight>
+    </ScrollView>
+  );
 }
+
+const color_touch = 'palegreen'
+const styles = StyleSheet.create({
+  view_out: { flex: 1, flexDirection: 'column', backgroundColor: '#fff' },
+  txt_pass: { marginRight: 10, marginLeft: 10, marginTop: 10 },
+  input_pass: {
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    borderColor: '#333',
+    borderWidth: 1,
+    fontSize: 18,
+  },
+  touch_changepass: {
+    marginBottom: 10,
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#ffceb5',
+    borderRadius: 8,
+  }
+})
