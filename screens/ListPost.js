b38c518579d5d8580ad28d12de9d9f45f210b0b6
Quach Nam <quachhungnam@gmail.com>
2020-06-19 11:10:49 +0700

    dinh viet lai list post va post detail

---
+++ screens/ListPost.js
@@ -0,0 +1,14 @@
+import React, { Component, useState, useEffect } from 'react';
+import { Text, View, TouchableHighlight, ScrollView, StyleSheet } from 'react-native';
+import PostYouBookFlatList from '../components/PostYouBookFlatList';
+import AsyncStorage from '@react-native-community/async-storage'
+import { FlatList } from 'react-native-gesture-handler';
+
+export default function ListPost(props) {
+    return(
+        <FlatList>
+            
+        </FlatList>
+    )
+}
+
