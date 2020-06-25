c0f44cc5f4ee4037c3e83ed6a8eacf8c413a46e2
Duyen Rice <duyenqt98@gmail.com>
2020-06-25 22:53:57 +0700

    Handle update account avatar function

--- src/screens/EditAvatarAccount.js
+++ src/screens/EditAvatarAccount.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   Dimensions,
   Alert,
@@ -10,6 +10,9 @@ import {
   Image,
 } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
+import {update_account_avatar} from '../api/account_api';
+
+import RNFetchBlob from 'rn-fetch-blob';
 
 export default class EditAvatarAccount extends Component {
   constructor(props) {
@@ -23,20 +26,44 @@ export default class EditAvatarAccount extends Component {
   openImagePicker() {
     const newData = this.state.images;
     ImagePicker.openPicker({
-      //multiple: true,
       mediaType: 'photo',
-    }).then(images1 => {
+    }).then(image => {
       let item = {
-        width: images1.width,
-        height: images1.height,
-        uri: images1.path,
-        id: Date.now(),
+        width: image.width,
+        height: image.height,
+        name: String(
+          image.filename || Math.floor(Math.random() * Math.floor(999999999)),
+        ),
+        filename:
+          image.filename ||
+          Math.floor(Math.random() * Math.floor(999999999)) + '.jpg',
+        type: image.type || 'image/jpeg',
+        uri: image.path,
+        data: RNFetchBlob.wrap(image.path),
       };
       newData.push(item);
-      this.setState({ images: newData });
-      console.log(this.state.images);
+      this.setState({images: newData});
     });
   }
+
+  updateAvatar = async () => {
+    update_account_avatar(
+      this.state.images,
+      this.props.route.params.account,
+      this.props.route.params.user_token,
+    )
+      .then(res => {
+        if (res.success === true) {
+          alert('Cập nhật thành công!');
+          this.props.navigation.navigate('YourAccountScreen');
+        } else {
+          alert('Cập nhật không thành công!');
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   render() {
     var width1 = Dimensions.get('window').width;
@@ -44,16 +71,16 @@ export default class EditAvatarAccount extends Component {
     if (this.state.images.length != 0) {
       a = (
         <Image
-          style={{ width: 100, height: 70 }}
-          source={{ uri: this.state.images[0] }}
+          style={{width: 100, height: 70}}
+          source={{uri: this.state.images[0]}}
         />
       );
     }
     return (
-      <ScrollView style={{ backgroundColor: '#fff' }}>
+      <ScrollView style={{backgroundColor: '#fff'}}>
         <FlatList
           data={this.state.images}
-          renderItem={({ item, index }) => {
+          renderItem={({item, index}) => {
             return (
               <TouchableHighlight
                 onLongPress={() => {
@@ -61,7 +88,7 @@ export default class EditAvatarAccount extends Component {
                     'Thông báo',
                     'Bạn có chắc chắn muốn xóa ảnh này?',
                     [
-                      { text: 'Không', onPress: () => { }, style: 'cancel' },
+                      {text: 'Không', onPress: () => {}, style: 'cancel'},
                       {
                         text: 'có',
                         onPress: () => {
@@ -71,7 +98,7 @@ export default class EditAvatarAccount extends Component {
                               newData.splice(i, 1);
                             }
                           }
-                          this.setState({ images: newData });
+                          this.setState({images: newData});
                         },
                         style: 'cancel',
                       },
@@ -91,7 +118,7 @@ export default class EditAvatarAccount extends Component {
                     height: (item.height * 1 * width1) / item.width,
                     width: 0.95 * width1,
                   }}
-                  source={{ uri: item.uri }}
+                  source={{uri: item.uri}}
                 />
               </TouchableHighlight>
             );
@@ -111,7 +138,7 @@ export default class EditAvatarAccount extends Component {
             borderRadius: 8,
           }}
           onPress={this.openImagePicker}>
-          <Text style={{ textAlign: 'center' }}>Chọn ảnh</Text>
+          <Text style={{textAlign: 'center'}}>Chọn ảnh</Text>
         </TouchableHighlight>
         <TouchableHighlight
           underlayColor={'#ffceb56e'}
@@ -127,20 +154,10 @@ export default class EditAvatarAccount extends Component {
             borderRadius: 8,
           }}
           onPress={() => {
-            //alert(JSON.stringify(this.getMyStringValue()));
-            this.getMyStringValue();
+            this.updateAvatar();
           }}>
-          <Text style={{ textAlign: 'center' }}>Cập nhật</Text>
+          <Text style={{textAlign: 'center'}}>Cập nhật</Text>
         </TouchableHighlight>
-        {/* <TouchableHighlight
-                        disabled={this.state.images.length == 0 ? true : false}
-                        style={{marginBottom: 10, marginTop: 10, marginLeft: 10, marginRight: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: 'lightblue', borderRadius: 10}}
-                        onPress={() => {
-                            this.setStringValue();
-                        }}
-                    >
-                    <Text style={{textAlign: 'center'}}>Đăng bài1</Text>
-                </TouchableHighlight>  */}
       </ScrollView>
     );
   }
